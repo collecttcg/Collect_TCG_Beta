04a61df0dbc554865d3af88d50f5629aeba402a2
collecttcg <z3313514@gmail.com>
2026-09-17 00:52:12 +0900

    2026-09-17-v04 improve One Piece logo and light-mode tiles

--- beta/src/main.js
+++ beta/src/main.js
@@ -1,5 +1,5 @@
 import { createProductionRuntime } from './app/production-runtime.js';
-import { registerFeatures } from './app/register-features.js?v=2026-09-17-v03';
+import { registerFeatures } from './app/register-features.js?v=2026-09-17-v04';
 import { initializeApp } from './app/initialize.js';
 import { setup as setup1 } from './ui/enhancement-1.js';
 import { setup as setup2 } from './ui/enhancement-2.js';
