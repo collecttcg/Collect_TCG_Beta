22a8493ebb08d544e86a9e09a73abeb3da1bf20e
collecttcg <z3313514@gmail.com>
2026-09-17 22:33:52 +0900

    Beta 2026-09-17-v16: refresh Insights intent module cache

--- beta/src/app/register-features.js
+++ beta/src/app/register-features.js
@@ -27,7 +27,7 @@ import { register as register24 } from '../features/content/giveaways.js';
 import { register as register25 } from '../features/content/showcase.js';
 import { register as register26 } from '../features/content/giveaway-pages.js';
 import { register as register27 } from '../features/owner/insights.js';
-import { register as registerInsightsIntentRates } from '../features/owner/insights-intent-rates.js?v=2026-09-17-v13';
+import { register as registerInsightsIntentRates } from '../features/owner/insights-intent-rates.js?v=2026-09-17-v16';
 import { register as registerInsightsDashboard } from '../features/owner/insights-dashboard.js?v=2026-09-17-v15';
 import { register as register28 } from '../features/content/reviews.js';
 import { register as register29 } from '../features/content/information.js';
