5a86580ff0f05f74ac15032005b328695f07a952
collecttcg <z3313514@gmail.com>
2026-09-18 11:06:19 +0900

    Beta 2026-09-18-v05: refresh details module cache

--- beta/src/app/register-features.js
+++ beta/src/app/register-features.js
@@ -42,7 +42,7 @@ import { register as register37 } from '../features/owner/bulk-metadata.js';
 import { register as register38 } from '../features/owner/history.js';
 import { register as register39 } from '../features/owner/export.js';
 import { register as register40 } from '../features/owner/add.js';
-import { register as register41 } from '../features/cards/details.js';
+import { register as register41 } from '../features/cards/details.js?v=2026-09-18-v05';
 import { register as register42 } from '../features/owner/editor.js';
 import { register as register43 } from '../app/theme.js';
 import { register as register44 } from '../app/startup.js';
