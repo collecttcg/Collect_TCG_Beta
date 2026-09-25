f764c0af9636ae3fef29ee2a707b53549e55ac81
collecttcg <z3313514@gmail.com>
2026-09-25 22:19:08 +0900

    Beta v08: update module cache references

--- beta/src/app/register-features.js
+++ beta/src/app/register-features.js
@@ -31,7 +31,7 @@ import { register as registerInsightsIntentRates } from '../features/owner/insig
 import { register as registerInsightsDashboard } from '../features/owner/insights-dashboard.js?v=2026-09-25-v05';
 import { register as register28 } from '../features/content/reviews.js';
 import { register as register29 } from '../features/content/information.js';
-import { register as register30 } from '../features/social/posts.js?v=2026-09-25-v05';
+import { register as register30 } from '../features/social/posts.js?v=2026-09-25-v08';
 import { register as register31 } from '../features/owner/quality.js';
 import { register as register32 } from '../features/owner/tools.js?v=2026-09-18-v15';
 import { register as register33 } from '../features/owner/bulk-status.js?v=2026-09-19-v02';
@@ -42,7 +42,7 @@ import { register as register37 } from '../features/owner/bulk-metadata.js';
 import { register as register38 } from '../features/owner/history.js';
 import { register as register39 } from '../features/owner/export.js';
 import { register as register40 } from '../features/owner/add.js';
-import { register as register41 } from '../features/cards/details.js?v=2026-09-25-v07';
+import { register as register41 } from '../features/cards/details.js?v=2026-09-25-v08';
 import { register as register42 } from '../features/owner/editor.js';
 import { register as register43 } from '../app/theme.js';
 import { register as register44 } from '../app/startup.js?v=2026-09-24-v12';
