96c96e781c4ca357c091ad2951ddb9a9d94d3c01
collecttcg <z3313514@gmail.com>
2026-09-26 16:46:02 +0900

    Beta 2026-09-26-v04: refresh information module

--- beta/src/app/register-features.js
+++ beta/src/app/register-features.js
@@ -30,7 +30,7 @@ import { register as register27 } from '../features/owner/insights.js';
 import { register as registerInsightsIntentRates } from '../features/owner/insights-intent-rates.js?v=2026-09-17-v16';
 import { register as registerInsightsDashboard } from '../features/owner/insights-dashboard.js?v=2026-09-25-v05';
 import { register as register28 } from '../features/content/reviews.js';
-import { register as register29 } from '../features/content/information.js';
+import { register as register29 } from '../features/content/information.js?v=2026-09-26-v04';
 import { register as register30 } from '../features/social/posts.js?v=2026-09-25-v08';
 import { register as register31 } from '../features/owner/quality.js';
 import { register as register32 } from '../features/owner/tools.js?v=2026-09-18-v15';
