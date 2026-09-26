902b31e7169144075a8be4cb6d8ce6781bc91eb3
collecttcg <z3313514@gmail.com>
2026-09-26 22:48:25 +0900

    Beta 2026-09-26-v17: keep automatic inventory sort out of clean URLs

--- beta/src/app/routing.js
+++ beta/src/app/routing.js
@@ -498,7 +498,7 @@ function updateListingUrlFromControls(){
     const sort = appContext.safeUrlFilterText(appContext.$("sortBy")?.value, 24);
     const defaultSort = route === "sold"
       ? "recent-sold"
-      : (route === "collection" ? "custom" : "name");
+      : (route === "inventory" ? "format-condition" : (route === "collection" ? "custom" : "name"));
     if(sort && sort !== defaultSort) params.set("sort", sort);
 
     if(appContext.activeQuickFilter && appContext.activeQuickFilter !== "all"){
